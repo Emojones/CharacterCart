e4bd61c9ff57a69989c138ac4579fb45cf94a090
Emojones <emonomics@gmail.com>
2018-04-14 14:18:10 -0400

    references and nuget packages setup

--- CharacterCart.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CharacterCart.DAL
-{
-    public class Class1
-    {
-    }
-}
--- CharacterCart.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CharacterCart.Tests
-{
-    public class Class1
-    {
-    }
-}
