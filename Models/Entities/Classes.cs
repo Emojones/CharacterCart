2ff263447571303373384292ce42d4a10ecdeffd
Emojones <emonomics@gmail.com>
2018-04-14 20:53:10 -0400

    added references and started building model

---
+++ Models/Entities/Classes.cs
@@ -0,0 +1,16 @@
+﻿using CharacterCart.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterCart.Models.Entities
+{
+    public class Classes : EntitiesBase
+    {
+        //name
+        //isarcane caster
+        //is divine caster
+        //img url
+
+    }
+}
---
+++ Models/Entities/Weapons.cs
@@ -0,0 +1,14 @@
+﻿using CharacterCart.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterCart.Models.Entities
+{
+   public class Weapons : EntitiesBase
+    {
+        //name
+        //imgurl
+        //isranged
+    }
+}
