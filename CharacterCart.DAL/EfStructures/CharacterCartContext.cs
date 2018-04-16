6c1dcfac7072d7d7043f1cfabb746b2ea0f92e0e
Emojones <emonomics@gmail.com>
2018-04-16 19:52:08 -0400

    setup db context

---
+++ CharacterCart.DAL/EfStructures/CharacterCartContext.cs
@@ -0,0 +1,36 @@
+﻿using CharacterCart.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterCart.DAL.EfStructures
+{
+    public class CharacterCartContext : DbContext
+    {
+        public CharacterCartContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<CharacterRaces> CharacterRaces { get; set; }
+        public DbSet<Classes> Classes { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Weapons> Weapons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Classes>(entity =>
+            {
+                entity.Property(e => e.IsArcaneCaster).HasColumnType("bit").HasDefaultValue(false);
+                entity.Property(e => e.IsDivineCaster).HasColumnType("bit").HasDefaultValue(false);
+            });
+
+            modelBuilder.Entity<Weapons>(entity =>
+            {
+                entity.Property(e => e.IsRanged).HasColumnType("bit").HasDefaultValue(false);
+            });
+
+        }
+    }
+}
---
+++ CharacterCart.DAL/EfStructures/CharacterCartContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterCart.DAL.EfStructures
+{
+    public class CharacterCartContextFactory : IDesignTimeDbContextFactory<CharacterCartContext>
+    {
+        public CharacterCartContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CharacterCartContext>();
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CharacterCart;Trusted_Connection=True;   MultipleActiveResultSets=true;";
+
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure()).ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            return new CharacterCartContext(optionsBuilder.Options);
+        }
+    }
+}
